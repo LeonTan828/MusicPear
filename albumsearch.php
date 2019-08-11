95f348893d894228e8d31c594da240df41c8a7d4
Leon <yltan828@gmail.com>
2019-08-11 17:58:43 -0500

    new php files

---
+++ albumsearch.php
@@ -0,0 +1,24 @@
+<?php
+    $con = mysqli_connect('localhost', 'root', 'root', 'musicpear');
+
+    // Check to seee if the connection was successful
+    if (mysqli_connect_errno())
+    {
+        echo("connection failed");
+        exit();
+    }
+
+    $searchterm = $_POST["searchterm"];
+
+    $albumsearchquery = "SELECT songID FROM Song WHERE album = '" . $searchterm . "';";
+    $albumsearch = mysqli_query($con, $albumsearchquery) or die("album search failed");
+
+    if (mysqli_num_rows($albumsearch) == 0)
+    {
+        echo("No such album exist");
+        exit();
+    }
+
+    echo("0");
+    // NOTE figure out how to return the search results
+?>
---
+++ titlesearch.php
@@ -0,0 +1,24 @@
+<?php
+    $con = mysqli_connect('localhost', 'root', 'root', 'musicpear');
+
+    // Check to seee if the connection was successful
+    if (mysqli_connect_errno())
+    {
+        echo("connection failed");
+        exit();
+    }
+
+    $searchterm = $_POST["searchterm"];
+
+    $titlesearchquery = "SELECT songID FROM Song WHERE title = '" . $searchterm . "';";
+    $titlesearch = mysqli_query($con, $titlesearchquery) or die("title search failed");
+
+    if (mysqli_num_rows($titlesearch) == 0)
+    {
+        echo("No such title exist");
+        exit();
+    }
+
+    echo("0");
+    // NOTE figure out how to return the search results
+?>
