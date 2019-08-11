be569414c7f5b07b91d7d022498660c29d835703
Leon <yltan828@gmail.com>
2019-08-11 17:20:59 -0500

    typing

--- searchSong.cs
+++ searchSong.cs
@@ -2,5 +2,31 @@
 //search by title
 //search by album
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unityengine.UI;
 
-Using 
+public class SearchSong : MonoBehaviour {
+
+    public InputField searchField;
+
+    public Button artistButton;
+    public Button titleButton;
+    public Button albumButton;
+
+    public void Callartistsearch()
+    {
+
+    }
+
+    public void Calltitlesearch()
+    {
+
+    }
+
+    public void Callalbumsearch()
+    {
+
+    }
+}
