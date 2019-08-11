57a27fd0ebcf0c40dc1743e1e677b117ea0bb38c
davidwstewart <dstewart3@wisc.edu>
2019-08-11 15:58:16 -0500

    updated register.php

--- register.php
+++ register.php
@@ -1,16 +1,16 @@
 <?php
-    $con = mysqli_connect('localhost', 'root', 'root', 'musicpear');
+    $con = mysqli_connect('localhost:3307', 'root', 'root', 'musicpear');
 
     //check to seee if the connection was successful
     if (mysqli_connect_errno())
     {
         echo "connection failed";
-        exit();
+        exit()
     }
 
+    $name = $_POST["name"];
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $name = $_POST["name"];
 
     //check if username is already taken
     $usernamecheckquery = "SELECT userID FROM user WHERE userID='" . $username . "';";
@@ -27,4 +27,5 @@
     $insertuserquery = "INSERT INTO user (username, password, userID) VALUES ('" . $username ."', '" . $password . "', '" . $name . "');";
     mysqli_query($con, $insertuserquery) or die("Insert user failed");
 
+    echo("0");
  ?>
