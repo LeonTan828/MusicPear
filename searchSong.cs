569f208460cb1fc669d4c3e0171c41bb8ede9f1f
Leon <yltan828@gmail.com>
2019-08-11 17:31:34 -0500

    completed search cs file

--- searchSong.cs
+++ searchSong.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unityengine.UI;
+using UnityEngine.SceneManagement;
 
 public class SearchSong : MonoBehaviour {
 
@@ -17,16 +18,76 @@ public class SearchSong : MonoBehaviour {
 
     public void Callartistsearch()
     {
-
+        StartCoroutine(ArtistSearch());
     }
 
     public void Calltitlesearch()
     {
-
+        StartCoroutine(TitleSearch());
     }
 
     public void Callalbumsearch()
     {
+        StartCoroutine(AlbumSearch());
+    }
 
+    IEnumerator ArtistSearch() 
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("searchterm", searchField.text);
+        // NOTE: Remember to change the PHP
+        WWW www = new WWW("http://localhost/sqlconnect/artistsearch.php", form);
+        yield return www;
+        
+        if (www.text[0] == '0')
+        {
+            Debug.Log("Successfully found songs");
+            SceneManager.LoadScene() // NOTE: need to check what scene
+        }
+        else
+        {
+            Debug.Log("Error: Couldn't search " + www.text);
+        }
+    }
+
+    IEnumerator TitleSearch() 
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("searchterm", searchField.text);
+        // NOTE: Remember to change the PHP
+        WWW www = new WWW("http://localhost/sqlconnect/titlesearch.php", form);
+        yield return www;
+        
+        if (www.text[0] == '0')
+        {
+            Debug.Log("Successfully found songs");
+            SceneManager.LoadScene() // NOTE: need to check what scene
+        }
+        else
+        {
+            Debug.Log("Error: Couldn't search " + www.text);
+        }
+    }
+
+    IEnumerator AlbumSearch() 
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("searchterm", searchField.text);
+        // NOTE: Remember to change the PHP
+        WWW www = new WWW("http://localhost/sqlconnect/albumsearch.php", form);
+        yield return www;
+        
+        if (www.text[0] == '0')
+        {
+            Debug.Log("Successfully found songs");
+            SceneManager.LoadScene() // NOTE: need to check what scene
+        }
+        else
+        {
+            Debug.Log("Error: Couldn't search " + www.text);
+        }
     }
 }
