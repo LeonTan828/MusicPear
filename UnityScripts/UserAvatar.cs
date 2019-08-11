c0f43d24d95b67a9910c798a8ddb77c22fef1ec0
kiminth <43854877+kiminth@users.noreply.github.com>
2019-08-10 20:35:12 -0500

    Add files via upload

---
+++ UnityScripts/UserAvatar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAvatar : MonoBehaviour
+{
+    public static UserAvatar instance;
+
+    private void Awake()
+    {
+        if (instance == null || gameObject == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } else
+        {
+            Destroy(gameObject);
+            
+        }
+    }
+}
---
+++ UnityScripts/UserGlobal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGlobal : MonoBehaviour
+{
+    // Global Saves
+    public List<Playlist> userPlaylists = new List<Playlist>();
+    public string userName;
+    public string userPassword;
+    public string userUserName;
+
+    public static UserGlobal Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
