7783a11ea1b6639b8f64416e056873cb4386ab27
Leon <yltan828@gmail.com>
2019-08-12 12:11:41 -0500

    minor

--- MusicPear/Assets/Scripts/DBManager.cs
+++ MusicPear/Assets/Scripts/DBManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class DBManager {
 
-    public static string username
+    public static string username;
 
     public static bool LoggedIn { get { return username != null;} }
 
