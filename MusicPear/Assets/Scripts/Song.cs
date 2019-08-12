f7cac6e6319247b2e0586dce8b801b9c7d93e492
Leon <yltan828@gmail.com>
2019-08-12 12:25:08 -0500

    new song file

--- MusicPear/Assets/Scripts/Song.cs
+++ MusicPear/Assets/Scripts/Song.cs
@@ -7,4 +7,45 @@ public class Song : MonoBehaviour
     private string name;
     private string artist;
     private string album;
+    private string songID;
+
+    public Song ()
+    {
+        songID = null;
+    }
+
+    public void setSongID(string currID)
+    {
+        songID = currID;
+    }
+    public void findData()
+    {
+        StartCoroutine(getData());
+    }
+
+    IEnumerator getData()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("searchID", songID);
+        // NOTE: Remember to change the PHP
+        WWW www = new WWW("http://localhost/sqlconnect/songinfoshow.php", form);
+        yield return www;
+        
+        if (www.text[0] == '0')
+        {
+            string temp = www.text;
+            string[] words = temp.Split(' ');
+
+            Debug.Log("Successfully found songs");
+
+            name = words[1];
+            artist = words[2];
+            album = words[3];
+        }
+        else
+        {
+            Debug.Log("Error: Couldn't search: " + www.text);
+        }
+    }
+
 }
